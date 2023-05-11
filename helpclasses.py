9eeb1ef59d18b523aaffd42437737a8646eea6a7
Emanuel181 <emanuel.rusu03@e-uvt.ro>
2023-05-11 17:49:22 +0300

    first push

---
+++ helpclasses.py
@@ -0,0 +1,17 @@
+from kivy.uix.screenmanager import Screen
+
+
+class WelcomeScreen(Screen):
+    pass
+
+
+class EnterUsername(Screen):
+    pass
+
+
+class StoreUserClass(Screen):
+    pass
+
+
+class MainScreen(Screen):
+    pass
--- login.py
+++ login.py
@@ -7,181 +7,22 @@ from kivymd.uix.button import MDFlatButton
 from kivy.storage.jsonstore import JsonStore
 from main import MainApp
 from main import GraphCreationClass
-
-helpstr = '''
-
-ScreenManager:
-    WelcomeScreen:
-    UsernameScreen:
-    DOB:
-    MainScreen:
-    
-<WelcomeScreen>:
-    name : 'welcomescreen'
-    MDLabel:
-        text:'Primul tau pas spre o viata mai organizata!'
-        font_style: 'H2'
-        halign: 'center'
-        pos_hint: {'center_y':0.65}
-        
-    MDFloatingActionButton:
-        icon:'account-plus-outline'
-        md_bg_color:app.theme_cls.primary_color
-        user_font_size : '60sp'
-        pos_hint: {'center_x':0.5,'center_y':0.32}
-        on_press:
-            root.manager.current = 'usernamescreen'
-            root.manager.transition.direction = 'left'
-    MDProgressBar:
-        value:30
-        pos_hint:{'center_y' : 0.02}
-        
-<UsernameScreen>
-    name:'usernamescreen'
-    MDFloatingActionButton:
-        icon: 'arrow-left'
-        md_bg_color:app.theme_cls.primary_color
-        pos_hint: {'center_x':0.1,'center_y':0.1}
-        user_font_size : '45sp'
-        on_press:
-            root.manager.current = 'welcomescreen'
-            root.manager.transition.direction = 'right'
-            
-    MDFloatingActionButton:
-        id:disabled_button
-        disabled: True
-        icon: 'arrow-right'
-        md_bg_color:app.theme_cls.primary_color
-        pos_hint: {'center_x':0.9,'center_y':0.1}
-        user_font_size : '45sp'
-        on_press:
-            root.manager.current = 'dob'
-            root.manager.transition.direction = 'left'
-            
-    MDProgressBar:
-        value:60
-        pos_hint: {'center_y':0.02}
-
-    MDLabel:
-        text:'Username'
-        font_style: 'H2'
-        halign: 'center'
-        pos_hint : {'center_y':0.85}
-
-    MDTextField:
-        id:username_text_fied
-        pos_hint: {'center_x':0.5,'center_y':0.6}
-        size_hint: (0.7,0.1)
-        hint_text : 'username'
-        helper_text: 'Required'
-        helper_text_mode: 'on_error'
-        icon_right: 'account'
-        icon_right_color: app.theme_cls.primary_color
-        required : True
-
-    MDFloatingActionButton:
-        icon:'account-plus'
-        md_bg_color:app.theme_cls.primary_color
-        pos_hint: {'center_x':0.5,'center_y':0.35}
-        user_font_size: '50sp'
-        on_press: app.check_username()
-
-<DOB>:
-    name:'dob'
-    MDLabel:
-        text:'Esti la un pas de a folosi aplicatia!'
-        font_style: 'H2'
-        halign: 'center'
-        pos_hint: {'center_y':0.75} 
-
-    MDRaisedButton:
-        id:date_picker
-        text:'Apasa pentru a finaliza'
-        user_font_size : '70sp'
-        pos_hint : {'center_x':0.5,'center_y':0.4}
-        on_press: app.get_date()
-
-    MDFloatingActionButton:
-        icon:'arrow-left'
-        md_bg_color:app.theme_cls.primary_color
-        pos_hint: {'center_x':0.1,'center_y':0.1}
-        user_font_size: '45sp'
-        on_press: root.manager.current = 'usernamescreen'
-        
-    MDFloatingActionButton:
-        id: second_disabled
-        disabled: False
-        icon:'arrow-right'
-        md_bg_color:app.theme_cls.primary_color
-        pos_hint: {'center_x':0.9,'center_y':0.1}
-        user_font_size: '45sp'
-        on_press: root.manager.current = 'mainscreen'
-
-<MainScreen>:
-    name : 'mainscreen'
-    MDLabel:
-        id:profile_name
-        text:'main screen'
-        font_style : 'H2'
-        halign : 'center'
-        pos_hint : {'center_y':0.7}
-
-    MDFloatingActionButton:
-        id: second_disabled
-        disabled: False
-        icon:'arrow-right'
-        md_bg_color:app.theme_cls.primary_color
-        pos_hint: {'center_x':0.9,'center_y':0.1}
-        user_font_size: '45sp'
-        on_press: app.stop()
-
-'''
-
-
-class WelcomeScreen(Screen):
-    pass
-
-
-class UsernameScreen(Screen):
-    pass
-
-
-class DOB(Screen):
-    pass
-
-
-class MainScreen(Screen):
-    pass
+from helpstring import helpingstring
+from helpclasses import WelcomeScreen, EnterUsername, StoreUserClass, MainScreen
 
 
 sm = ScreenManager()
 sm.add_widget(WelcomeScreen(name='welcomescreen'))
-sm.add_widget(UsernameScreen(name='usernamescreen'))
-sm.add_widget(DOB(name='dob'))
+sm.add_widget(EnterUsername(name='usernamescreen'))
+sm.add_widget(StoreUserClass(name='storeuserclass'))
 sm.add_widget(MainScreen(name='main_screen'))
 
 
-class NewApp(MDApp):
+class LoginAppKivy(MDApp):
     def build(self):
-        self.strng = Builder.load_string(helpstr)
+        self.strng = Builder.load_string(helpingstring)
         self.theme_cls.primary_palette = "DeepOrange"
         return self.strng
-
-    def check_username(self):
-        self.username_text = self.strng.get_screen('usernamescreen').ids.username_text_fied.text
-        username_check_false = True
-        try:
-            int(self.username_text)
-        except:
-            username_check_false = False
-        if username_check_false or self.username_text.split() == []:
-            cancel_btn_username_dialogue = MDFlatButton(text='Retry', on_release=self.close_username_dialogue)
-            self.dialog = MDDialog(title='Invalid Username', text="Please input a valid username", size_hint=(0.7, 0.2),
-                                   buttons=[cancel_btn_username_dialogue])
-            self.dialog.open()
-
-        else:
-            self.strng.get_screen('usernamescreen').ids.disabled_button.disabled = False
 
     def close_username_dialogue(self, obj):
         self.dialog.dismiss()
@@ -189,25 +30,46 @@ class NewApp(MDApp):
     def show_date_picker(self):
         pass
 
-    def get_date(self):
-        self.store.put('UserInfo', name=self.username_text)
-        self.username_changer()
+    def save_user_name(self):
+        self.store.put('UserInfo', name=self.username)
+        self.change_username_dynamically_on_screen()
 
-    def username_changer(self):
+    def change_username_dynamically_on_screen(self):
         self.strng.get_screen('mainscreen').ids.profile_name.text = f"Welcome {self.store.get('UserInfo')['name']}"
 
     def on_start(self):
-        self.store = JsonStore("userProfile.json")
+        self.store = JsonStore("user_name.json")
         try:
             if self.store.get('UserInfo')['name'] != "":
-                self.username_changer()
+                self.change_username_dynamically_on_screen()
                 self.strng.get_screen('mainscreen').manager.current = 'mainscreen'
 
         except KeyError:
             self.strng.get_screen('welcomescreen').manager.current = 'welcomescreen'
 
+    def check_for_valid_username(self):
+        self.username = self.strng.get_screen('usernamescreen').ids.username_text_fied.text
+        username_is_correct = True
+        try:
+            int(self.username)
+        except:
+            username_is_correct = False
+        if username_is_correct or self.username.split() == []:
+            cancel_btn_username_dialogue = MDFlatButton(text='Retry', on_release=self.close_username_dialogue)
+            self.dialog = MDDialog(title='Invalid Username', text="Retype username using letters and numbers(except "
+                                                                  "first position)",
+                                   size_hint=(0.7, 0.2),
+                                   buttons=[cancel_btn_username_dialogue])
+            self.dialog.open()
+
+        else:
+            self.strng.get_screen('usernamescreen').ids.disabled_button.disabled = False
+
+    def turn_off_button(self):
+        self.strng.get_screen('storeuserclass').ids.second_disabled.disabled = False
+
 
 if __name__ == "__main__":
-    NewApp().run()
+    LoginAppKivy().run()
     GraphCreationClass().run()
     MainApp().run()
